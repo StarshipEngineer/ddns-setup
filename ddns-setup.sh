039a88b6c9cb1afe0ecfa04f94d2f87b356699a7
Dan Shuman <dan.13.shuman@gmail.com>
2016-04-21 12:41:48 -0500

    Adds cron job

--- ddns-setup.sh
+++ ddns-setup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-sudo apt-get install ddclient
+apt-get update
+apt-get -y upgrade
+apt-get -y install ddclient libio-socket-ssl-perl
+
+#config copy
+
+#anything else there?
+sed -i '\crun_daemon="true"' /etc/default/ddclient
+
+sudo service ddclient start
+
+sudo service ddclient status
+
+cp /ddclient.cron /etc/cron.daily/ddclient
+sudo chmod +x /etc/cron.daily/ddclient
